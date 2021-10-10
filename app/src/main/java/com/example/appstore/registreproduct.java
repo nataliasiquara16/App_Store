de0b0d70d380d29841688c3e939884af5f52b6bf
Natalia Siquara <natalia.siquaara@hotmail.com>
2021-10-10 11:37:49 -0300

    Tirar e exibir foto 2

--- app/src/main/java/com/example/appstore/registreproduct.java
+++ app/src/main/java/com/example/appstore/registreproduct.java
@@ -2,14 +2,24 @@ package com.example.appstore;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class registreproduct extends AppCompatActivity {
 
@@ -18,7 +28,8 @@ public class registreproduct extends AppCompatActivity {
     private Button btn_load;
     private TextView tv_message;
     private ImageView iv_image;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +53,56 @@ public class registreproduct extends AppCompatActivity {
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            iv_image.setImageBitmap(imageBitmap);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bitmap takenImage = BitmapFactory.decodeFile(currentPhotoPath);
+            //colocando a imagem em uma imagemview
+            ImageView iv_image;
+            iv_image=findViewById(R.id.iv_image);
+            iv_image.setImageBitmap(takenImage);
+
+            //mostrando o nome do arquivo no text view
+            TextView tv_message;
+            tv_message= findViewById(R.id.tv_message);
+            tv_message.setText(currentPhotoPath);
         }
     }
-
-    // chamando uma intent para capturar uma foto
+// método para salvar o arquivo
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // criando o arquivo onde vai ser salvo as imagens
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // caso der erro
+                Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+            }
+            // se o arquivo for salvo com sucesso
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.appstore.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // criando o nome do arquivo
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  //prefixo
+                ".jpg",         //sufixo
+                storageDir      //diretorio
+        );
+
+        // salvando o arquivo
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
